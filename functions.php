1d22f16241fbb4fc5364b45166ede247ae87d65c
Thomas Gollenia <thomas@gollenia.at>
2024-01-16 05:06:17 +0000

    added mime types

--- functions.php
+++ functions.php
@@ -8,4 +8,13 @@ add_action('wp_head', function () {
 	echo '<link rel="apple-touch-icon" type="image/png" sizes="180x180" href="' . get_stylesheet_directory_uri() . '/favicons/favicon_' . get_locale() . '_180.png" />';
 	echo '<link rel="icon" type="image/png" sizes="32x32" href="' . get_stylesheet_directory_uri() . '/favicons/favicon_' . get_locale() . '_32.png" />';
 	echo '<link rel="icon" type="image/png" sizes="16x16" href="' . get_stylesheet_directory_uri() . '/favicons/favicon_' . get_locale() . '_16.png" />';
+	echo '<link rel="icon" type="image/x-icon" href="/assets/favicon.svg">';
 }, 100);
+
+function custom_upload_mimes( $existing_mimes ) {
+	$existing_mimes['webp'] = 'image/webp';
+    $existing_mimes['ico'] = 'image/x-icon';
+    $existing_mimes['svg'] = 'image/svg+xml';
+	return $existing_mimes;
+}
+add_filter( 'mime_types', 'custom_upload_mimes' );
